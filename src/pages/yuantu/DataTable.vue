0bf3e21d85a6017c81a48662e03e85defd2a482e
Zxw <1046137644@qq.com>
2020-09-07 08:58:42 +0800

    2020/9/7

---
+++ src/pages/yuantu/DataTable.vue
@@ -0,0 +1,58 @@
+<template>
+    <el-table
+            :data="tableData"
+            style="width: 100%">
+        <el-table-column type="expand">
+            <template slot-scope="props">
+                <el-form label-position="left" inline class="demo-table-expand">
+                    <el-form-item label="排班账号状态">
+                        <span>{{ props.row.name }}</span>
+                    </el-form-item>
+                    <el-form-item label="prios账号状态">
+                        <span>{{ props.row.shop }}</span>
+                    </el-form-item>
+                    <el-form-item label="图文">
+                        <span>{{ props.row.shopId }}</span>
+                    </el-form-item>
+                    <el-form-item label="视频">
+                        <span>{{ props.row.category }}</span>
+                    </el-form-item>
+<!--                    <el-form-item label="店铺地址">-->
+<!--                        <span>{{ props.row.address }}</span>-->
+<!--                    </el-form-item>-->
+<!--                    <el-form-item label="商品描述">-->
+<!--                        <span>{{ props.row.desc }}</span>-->
+<!--                    </el-form-item>-->
+                </el-form>
+            </template>
+        </el-table-column>
+        <el-table-column
+                label="医生姓名"
+                prop="doctName">
+        </el-table-column>
+        <el-table-column
+                label="医管账号"
+                prop="phoneNum">
+        </el-table-column>
+<!--        <el-table-column-->
+<!--                label="描述"-->
+<!--                prop="desc">-->
+<!--        </el-table-column>-->
+    </el-table>
+</template>
+
+<script>
+    export default {
+        name: "DataTable",
+        props:['tableData'],
+        data() {
+            return {
+                tableData: this.tableData
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ module.exports = {
         // proxy: 'http://121.199.14.154:8080',
         proxy: {//跨域设置
             '/api': {
-                // target: 'http://localhost:8081',
-                'target': 'http://121.199.14.154:8080',
+                target: 'http://localhost:8085',
+                // 'target': 'http://121.199.14.154:8080',
                 pathRewrite: {'^/api': ''}
             }
         },
