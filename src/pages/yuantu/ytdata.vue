21376ae6e1e36eaa17a61b4154ea3bf8e5ca6679
Zxw <1046137644@qq.com>
2020-10-15 00:08:20 +0800

    2020/10/14

--- src/pages/yuantu/ytdata.vue
+++ src/pages/yuantu/ytdata.vue
@@ -1,44 +1,51 @@
 <template>
     <div class="yadata">
         <el-row class="data-el-row">
-            <el-col :span="12" >
+            <el-col :span="12">
                 <el-row>
-                    <el-col>
                     <div>>>>获取医管账号:</div>
-                    <el-select size="small" v-model="value" placeholder="请选择" @change="getOptionsValue(value)">
-                        <el-option
-                                v-for="(item,index) in cityOptions"
-                                :key="item.value"
-                                :label="item.label"
-                                :value="item.value">
-                        </el-option>
-                    </el-select>
-                    <el-button size="small" @click="getAccount">获取</el-button>
+                    <el-col :span="4">
+                        <el-select size="medium" v-model="value" placeholder="请选择" @change="getOptionsValue(value)">
+                            <el-option
+                                    v-for="(item,index) in cityOptions"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-input size="medium" clearable maxlength="11" placeholder="请输入排班医生手机号（默认青岛）"
+                                  v-model="inputValue"></el-input>
+                    </el-col>
+                    <el-col :span="3">
+                        <el-button size="medium" @click="getAccount">获取</el-button>
                     </el-col>
                 </el-row>
-              <el-row>
-                  <el-row>
-                      <div>>>>>一键排班</div>
-                  </el-row>
-                  <el-row>
-                      <el-checkbox-group v-model="checkList">
-                          <el-checkbox v-for="(list,index) of inquiry_list" :label="list" checked></el-checkbox>
-                      </el-checkbox-group>
-                  </el-row>
-                  <el-row class="data-schedule">
-                  <el-col :span="8">
-                  <el-input size="medium" clearable maxlength="11" placeholder="请输入排班医生手机号（默认青岛）" v-model="inputValue"></el-input>
-                  </el-col>
+                <el-row>
+                    <el-row>
+                        <div>>>>>一键排班</div>
+                    </el-row>
+                    <el-row>
+                        <el-checkbox-group v-model="checkList">
+                            <el-checkbox v-for="(list,index) of inquiry_list" :label="list" checked></el-checkbox>
+                        </el-checkbox-group>
+                    </el-row>
+                    <el-row class="data-schedule">
+                        <el-col :span="8">
+                            <el-input size="medium" clearable maxlength="11" placeholder="请输入排班医生手机号（默认青岛）"
+                                      v-model="inputValue"></el-input>
+                        </el-col>
 
-                  <el-col :span="8">
-                      <el-button size="small">排班</el-button>
-                  </el-col>
-                  </el-row>
-              </el-row>
+                        <el-col :span="8">
+                            <el-button size="medium">排班</el-button>
+                        </el-col>
+                    </el-row>
+                </el-row>
 
             </el-col>
-            <el-col :span="12" class="dataTable" v-show="true">
-                    <DataTable :tableData="doctorsData"></DataTable>
+            <el-col :span="8" class="dataTable" v-show="true">
+                <DataTable :tableData="doctorsData"></DataTable>
             </el-col>
 
             <!--        <span>一键创建医管账号</span>-->
@@ -58,17 +65,17 @@
     import DataTable from "./DataTable";
 
 
-    let inquiry_type = ["问诊","复诊","视频","视频复诊"];
+    let inquiry_type = ["问诊", "复诊", "视频", "视频复诊"];
     let citys = [{
-            value: '青岛',
-            label: '青岛'
-        }, {
-            value: '阿里云',
-            label: '阿里云'
-        }, {
-            value: '云南',
-            label: '云南'
-        }];
+        value: '青岛',
+        label: '青岛'
+    }, {
+        value: '阿里云',
+        label: '阿里云'
+    }, {
+        value: '云南',
+        label: '云南'
+    }];
     export default {
         name: "ytdata",
         components: {
@@ -80,9 +87,9 @@
                 cityOptions: citys,
                 value: '',
                 doctorsData: [],
-                inputValue : '',
+                inputValue: '',
                 checkList: [],
-                inquiry_list:inquiry_type
+                inquiry_list: inquiry_type
             }
         },
         methods: {
@@ -117,22 +124,27 @@
         padding-top: 3rem;
         font-size: 1rem;
 
-          .data-el-row {
-             div
-             {
-                 margin-bottom: 0.5rem;
-             }
-              .data-schedule {
-                  line-height: 2;
-              }
-          }
+        .data-el-row {
+            div {
+                margin-bottom: 0.5rem;
+            }
+
+            .data-schedule {
+                line-height: 2;
+            }
+
+            .el-col {
+                margin-right: 1rem;
+            }
+        }
+
         /*span {*/
         /*    margin-bottom: 1rem;*/
         /*    margin-top: 1rem;*/
         /*}*/
 
         button {
-            margin-left: 1rem;
+            /*margin-left: 1rem;*/
         }
 
     }
