60333cd802ec4d9a5c2d346e38b8ff030cde7e1a
Zxw <1046137644@qq.com>
2021-05-02 13:31:22 +0800

    2

---
+++ src/components/1.api_auto_demo/child/child1.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <span>child1</span>
+        <span>{{value}}</span>
+            <input type="text" v-model="value">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "child1",
+        data() {
+            return {
+                value: ''
+            }
+        }
+    }
+</script>
+
+<style scoped type="text/css" lang="scss">
+    input {
+        border: 1px solid black;
+    }
+</style>
