1260ed3d41b1c440c873224bab19d6c28acf4225
zxwzs <zhuxinwei@gdzskj.cn>
2022-11-25 10:59:54 +0800

    11.25

---
+++ src/pages/others/404.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="notfound">
+        <h1>404</h1>
+        <span>页面未找到，<router-link :to="{
+            path: '/',
+            replace: true
+        }"> 返回首页></router-link></span>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.notfound {
+    text-align: center;
+    width: 100%;
+    font-style: italic;
+    color: #999;
+    padding-top: 10%;
+
+    h1 {
+        font-weight: 200;
+        font-size: 100px;
+        padding-bottom: 20px;
+    }
+
+    span {
+        font-size: 14px;
+        font-style: normal;
+    }
+}
+</style>
