0b654e2dbd918f04424c05662ca03b2f1e81689e
zxwzs <zhuxinwei@gdzskj.cn>
2022-12-08 18:09:09 +0800

    update

--- src/components/3todo_list_demo/todo-content.vue
+++ src/components/3todo_list_demo/todo-content.vue
@@ -1,42 +1,46 @@
 <template>
     <div>
         <ul class="content">
-        <li>
-            <span>1</span>
-            <button class="content-li-button el-icon-circle-close"
-            ></button>
-        </li>
-    </ul>
+            <li v-for="(item,index) in li_lists" :key="index">
+                <span>{{item}}</span>
+                <button class="content-li-button el-icon-circle-close"></button>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "todoContent",
-        data() {
-            return {
-              
-            }
-        },
-        created() {
-        },
-        methods: {
-        
+export default {
+    name: "todoContent",
+    data() {
+        return {
+            li_lists: [
+               1,
+               2
+            ]
         }
+    },
+    created() {
+    },
+    methods: {
+        addLi
+
     }
+}
 </script>
 
 <style lang="scss" scoped>
-    .content {
-        margin-top: 5%;
+.content {
+    margin-top: 5%;
 
-        li {
-            margin-top: 3%;
-            display: flex;
-            .content-li-button {
-                margin: 0 -8px 0 auto;
-                padding: 0 8px;
-            }
+    li {
+        margin-top: 3%;
+        display: flex;
+
+        .content-li-button {
+            margin: 0 -8px 0 auto;
+            padding: 0 8px;
         }
     }
+}
 </style>
